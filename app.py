a1ba0616993d7f792a3547fe2c6ca3cd4d8832e0
Nova User <nova@quantumgroupgh.com>
2023-06-12 13:34:29 +0530

    inital setup -1

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/app/home')
+def hello():
+    return '<h1>Hello, Homepage !!! content for the homepage</h1>'
+
+@app.route('/app/aboutus')
+def about():
+    return '<h1>about us page of the company!!!</h1>'
+
+@app.route('/app/contactus')
+def contact():
+    return '<h1>contact us for more services</h1>'
+
+@app.route('/app/login/')
+def login():
+    return '<h1>login page </h1>'
+
+@app.route('/app/login/user')
+def login_user():
+    return '<h1>Hello, Amn... this is for testing purpose</h1>'
+
+
